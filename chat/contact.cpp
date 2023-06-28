981463fd098963716bf3036e73999074b0136023
mhrg777 <133667392+mhrg777@users.noreply.github.com>
2023-06-29 02:52:06 +0330

    this directory for completeing chat
    added this files for client

---
+++ chat/contact.cpp
@@ -0,0 +1,40 @@
+#include "contact.h"
+
+const QString Contact::USERS_FOLDER = "Users", Contact::GROUPS_FOLDER = "Groups", Contact::CHANNELS_FOLDER = "Channels";
+
+
+Contact::Contact(QString name, QObject *parent) : QObject(parent)
+{
+    this->name = name;
+    this->lastModified = "";
+}
+
+void Contact::setRecentMessage(QString newMessage) {
+    if(!newMessage.isEmpty())
+        this->recentMessage = newMessage;
+}
+QString Contact::getRecentMessage() {
+    return this->recentMessage;
+}
+
+QString Contact::getRecentMessageSigned() {
+    return "\n\n" + this->name + ": " + this->recentMessage;
+}
+
+QString Contact::toString() {
+    return getName();
+}
+
+QString Contact::getFileName() {
+    switch (type()) {
+        case User:
+            return USERS_FOLDER + "/" + name;
+        case Group:
+            return GROUPS_FOLDER + "/" + name;
+        case Channel:
+            return CHANNELS_FOLDER + "/" + name;
+        case None:
+        default:
+            return "";
+    }
+}
